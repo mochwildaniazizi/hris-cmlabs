90b34c112b0e9e202f41255284917d7b3fb1e414
awaaa <hawa.esanda@gmail.com>
2025-04-25 10:09:15 +0700

    dashboard

---
+++ polinemajoss/app/dashboard/page.tsx
@@ -0,0 +1,28 @@
+import { Sidebar } from "../../components/ui/sidebar"
+import { ChartAreaInteractive } from "../../components/ui/chart-area-interactive"
+import { DataTable } from "../../components/ui/data-table"
+import { SectionCards } from "../../components/ui/section-card"
+import { SiteHeader } from "../../components/ui/site-header"
+import { SidebarInset, SidebarProvider } from "../../components/ui/sidebar"
+import data from "./data.json"
+export default function Page() {
+  return (
+    <SidebarProvider>
+      <Sidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <SectionCards />
+              <div className="px-4 lg:px-6">
+                <ChartAreaInteractive />
+              </div>
+              <DataTable data={data} />
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
