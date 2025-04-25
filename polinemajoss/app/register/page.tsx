a0d26bea05022578c6e9d94f6581d1bc50285447
balqozz <145304918+balqozz@users.noreply.github.com>
2025-04-25 13:12:03 +0700

    register page

---
+++ polinemajoss/app/register/page.tsx
@@ -0,0 +1,9 @@
+import { RegisterForm } from "../../components/ui/register-form"
+
+export default function RegisterPage() {
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-gray-100">
+      <RegisterForm className="w-full max-w-md" />
+    </main>
+  )
+}
